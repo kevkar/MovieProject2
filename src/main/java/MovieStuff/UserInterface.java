aa25b0cc7c9157c9152631f204d391b499633e58
Kevin Karch <kevkar@ufl.edu>
2020-08-20 15:35:02 -0400

    changed fonts

--- src/main/java/MovieStuff/UserInterface.java
+++ src/main/java/MovieStuff/UserInterface.java
@@ -20,7 +20,7 @@ public class UserInterface implements Runnable {
     @Override
     public void run() {
         frame = new JFrame("Movie Time");
-        frame.setPreferredSize(new Dimension(750, 600));
+        frame.setPreferredSize(new Dimension(725, 450));
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -39,21 +39,31 @@ public class UserInterface implements Runnable {
 
         TextFileManipulator.textFileStuff(ListenerClass.getDB());
 
+        Font font = new Font("Helvetica", Font.PLAIN,21);
+
         GridLayout layout = new GridLayout(4, 3);
         container.setLayout(layout);
 
-        JLabel nameText = new JLabel("  Movie Name: ");
+        JLabel nameText = new JLabel("  Movie Name: ",SwingConstants.CENTER);
+        nameText.setFont(font);
         JTextField nameField = new JTextField();
-        JLabel idText = new JLabel("  Movie Length: ");
+        JLabel idText = new JLabel("  Movie Length: ",SwingConstants.CENTER);
+        idText.setFont(font);
         JTextField idField = new JTextField("Manually enter length here if adding fails");
         JButton removeButton = new JButton("Remove!");
+        removeButton.setFont(font);
         JButton movieInfoButton = new JButton("Movie info");
+        movieInfoButton.setFont(font);
         JButton clearDB = new JButton("Remove all Movies");
+        clearDB.setFont(font);
         JButton addButton = new JButton("Add!");
+        addButton.setFont(font);
         JLabel listMoviesText = new JLabel("List of Movies",SwingConstants.CENTER);
+        listMoviesText.setVerticalAlignment(JLabel.BOTTOM);
+        listMoviesText.setFont(font);
         JButton randomMovie = new JButton("Random Movie");
+        randomMovie.setFont(font);
         JLabel stuffToPrint = PrinterToConsole.printArea();
-
         MovieRecordListener listener = new MovieRecordListener(nameField, idField);
         addButton.addActionListener(listener);
         movieInfoButton.addActionListener(new MovieSiteListener());
