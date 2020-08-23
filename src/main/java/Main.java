e6f39c44db7e1900f848c5c8a3b8a4f8a0bb0d0e
Kevin Karch <kevkar@ufl.edu>
2020-08-23 15:04:00 -0400

    delete duplicated folder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,10 @@ import Testing.Test1;
 public class Main {
     public static void main (String args[]) throws Exception
     {
-       //Invoker.StartProgram();
-
-        Test1 tester = new Test1();
-        tester.removeMovieTest();
+       Invoker.StartProgram();
+       
+       // Test1 tester = new Test1();
+       // tester.removeMovieTest();
     }
 }
 
